9ed1bb1312a88b4fc9f8b944717b78913f71e7e3
jbardon <bardonjeremy1@gmail.com>
2015-09-29 11:46:00 +0200

    Init projects, need to fix compilation

---
+++ LokiServer/src/main/java/fr/alma/middleware/interfaces/IServerForum.java
@@ -0,0 +1,11 @@
+package fr.alma.middleware.interfaces;
+
+import java.rmi.*;
+
+public interface IServerForum extends Remote {
+
+	public ITopic getTopic(String title) throws RemoteException;
+	public List<String> getTopicList() throws RemoteException;
+	public void postTopic(String title, ITopic topic) throws RemoteException;
+}
+
